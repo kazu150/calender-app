97d81d2de7c9c0a96079da153b3f2e87a668b34b
Kazushi Isogai <kazu150@gmail.com>
2021-01-07 14:54:04 +0900

    先月、次月を表示する関数を定義

--- front/src/services/calendar.js
+++ front/src/services/calendar.js
@@ -29,3 +29,20 @@ export const isFirstDay = day => day.date() === 1;
 export const getMonth = ({year, month}) => {
     return dayjs(`${year}-${month}`)
 }
+
+export const getNextMonth = month => {
+    const day = getMonth(month).add(1, 'month');
+    return formatMonth(day);
+}
+
+export const getPreviousMonth = month => {
+    const day = getMonth(month).add(-1, 'month');
+    return formatMonth(day);
+}
+
+export const formatMonth = day => {
+    return {
+        year: day.year(),
+        month: day.month() + 1 
+    }
+}
